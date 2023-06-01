3e9d37576e08204a7e571a8bdbb3be163d0858dd
Keith Manilla <keithmanilla@gmail.com>
2023-06-01 19:45:20 +0300

    Setup MongoDB Atlas DB Server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import cors from "cors";
+import connect from './utilities/mongodb';
+import dbConnect from './utilities/mongodb';
 
-/**
- * Initializations.
- */
+// Initializations.
 const app = express();
 const port = 8000;
 
 // TODO: Connect to DB (MongoDB)
-
+dbConnect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -22,7 +22,6 @@ app.use(
 		methods: "GET, POST, PUT, DELETE",
 	})
 );
-
 
 // TODO: Import Routes.
 
